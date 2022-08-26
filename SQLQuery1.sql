0b7ca32efbf0285c6726f2bc25c96d5b0af622d3
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-26 22:39:35 +0530

    Ability to create a address book service in data base

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+--create address Book Service
+--UC1
+create database Address_Book_service1;
+---UC2
+
+create table Address_Book1(FName varchar(200),LName varchar(200),Address varchar(200),City varchar(200),State varchar(200),Zip varchar(200),PhoneNumber varchar(200),email varchar(200));
